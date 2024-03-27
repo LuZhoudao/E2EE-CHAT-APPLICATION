f6e2ed3b5e7859cf32613b00c3807f4299ee9863
Gabriel <conte1.gc@gmail.com>
2024-03-27 14:30:10 +0800

    Start Input Validation
    1. register.js: Client-side input validation
    Also learned a lot about the application in general works

--- comp3334-project-files/chat/webapp/static/js/register.js
+++ comp3334-project-files/chat/webapp/static/js/register.js
@@ -22,7 +22,56 @@ async function generateAndSetPublicKey() {
     document.getElementById('registrationForm').submit();
 }
 
+function validateInputClientSide(form) {
+    
+    /**
+     * Client-side input validation. Return true if all inputs are valid, false otherwise
+     */
+
+    const username = form.elements['username'].value;
+    const password = form.elements['password'].value;
+    const securityAnswer = form.elements['securityAnswer'].value;
+    const memorizedSecret = form.elements['memorizedSecret'].value;
+
+    // Check for empty fields
+    if (!username || !password || !securityAnswer || !memorizedSecret) {
+        alert('All fields are required.');
+        return false;
+    }
+
+    const n = 3
+    // Check for minimum length
+    if (username.length < n || password.length < n || securityAnswer.length < n || memorizedSecret.length < n) {
+        alert(`All fields should be at least ${n} characters long.`);
+        return false;
+    }
+
+    // Check that password and memorizedSecret are different
+    if (password == memorizedSecret) {
+        alert('Password and Memorized Secret must be different.');
+        return false;
+    }
+
+    // If all checks pass, return true
+    return true;
+
+}
+
+
 document.getElementById('registrationForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the form from submitting immediately
+
+    // Input Validation
+    form = event.target;
+    
+    clientSideValidation = validateInputClientSide(form); // Client-side input validation
+    
+    if (!clientSideValidation) { 
+        return;
+    }
+
+    // Server feels safe for SQL Injection and XSS because of prepared statements??
+    
     generateAndSetPublicKey();
+
 });
