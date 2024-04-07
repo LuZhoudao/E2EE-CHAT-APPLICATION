d391f4014e9bf7953c75e22475de106e6b699ea7
Shin Hyuk <shinhyuk.contact@gmail.com>
2024-04-07 17:28:30 +0800

    .

--- comp3334-project-files/chat/webapp/app.py
+++ comp3334-project-files/chat/webapp/app.py
@@ -105,6 +105,7 @@ def fetch_messages():
         return jsonify({'error': 'Internal Server Error'}), 500
 
 def fetch_messages_from_db(peer_id, last_message_id):
+    
     query = """
     SELECT message_id, sender_id, receiver_id, ciphertext, iv, hmac, created_at 
     FROM messages 
@@ -113,6 +114,8 @@ def fetch_messages_from_db(peer_id, last_message_id):
         AND message_id > %s
     ORDER BY message_id ASC
     """
+    print("fetchmessage being called!!") #test
+    
     values = (session['user_id'], peer_id, peer_id, session['user_id'], last_message_id)
     
     cur = mysql.connection.cursor()
@@ -182,7 +185,6 @@ def get_latest_iv(peer_id):
 
     try:
         cur = mysql.connection.cursor()
-        # Query to find the IV of the latest message between the two users
         query = """
             SELECT iv
             FROM messages
@@ -194,13 +196,13 @@ def get_latest_iv(peer_id):
         result = cur.fetchone()
         cur.close()
 
-        # If a message is found, return its IV as hexadecimal, otherwise return '-1'
-        # Accessing the first item of the tuple directly
-        latest_iv = result[0] if result else '-1'
-        return jsonify({'iv': latest_iv.hex() if latest_iv != '-1' else latest_iv}), 200
+        # If a message is found, return its IV, otherwise return the base64-encoded initial IV
+        latest_iv_base64 = result[0] if result else "AAAAAAAAAAAAAAAAAAAAAA=="
+        return jsonify({'iv': latest_iv_base64}), 200
     except Exception as e:
         print(f"Error fetching the latest IV: {e}")
         return jsonify({'error': 'Internal Server Error'}), 500
+
 
 
 @app.route('/erase_chat', methods=['POST'])
